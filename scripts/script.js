9ee984ec5b740d45db774c90057eb7bd98dba239
JosanaKla <josana.0205@gmail.com>
2022-06-09 10:13:09 -0300

    feat and style: added and stylized ion-icon checkmark and js initialization

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+function finalPrice() {
+  var firstDish = preçoVindoDoPrato; 
+  var drink = preçoVindoDaBebida;
+  var desert = preçoVindoDaSobremesa;
+
+  var sum = firstDish + drink + desert;
+  alert("O preço final é: " + sum);
+}
+*/
+
+// Para ter certeza que 1 item de cada sessão está selecionado:
+
+/*
+if(selecao DIV firstDish === 1 && drink === 1 && desert === 1) {
+  faz o botão mudar para 'fechar pedido';
+}
+*/
